59d6430d771ad2865d8c88b04c920f9866a65ea2
Mino <m.randriamanivo@gmail.com>
2025-08-03 22:58:30 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -87,7 +87,7 @@ const ASSETS = [
   '/russian-torfl.json', // New JSON file for Chinese Simplified
   '/icon-512.png',
   '/icon-192.png',
-  '/favicon.webp'
+  '/favicon.ico'
 ];
 
 // Logging helper
